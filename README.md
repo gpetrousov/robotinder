907a3fa466dab13c69c727a807f7b2f072977ffb
Ioannis Petrousov <petrousov@gmail.com>
2018-01-10 23:05:20 +0100

    First version of swiper

---
+++ README.md
@@ -0,0 +1,20 @@
+# robotinder
+
+
+![alt text](https://i.imgur.com/ET1AWbz.gif "Logo Title why_the_hell_not")
+
+----
+
+Automates the right swiping process on tinder.
+
+----
+## usage
+1. Add your facebook credentials.
+2. Run the ruby script
+
+----
+## Similar projects (to mention a few)
+
+* https://www.kickstarter.com/projects/1424283429/tinda-finger
+* http://metro.co.uk/2015/02/10/say-goodbye-to-tinder-thumb-heres-an-app-that-will-automatically-swipe-right-on-over-15000-people-a-day-5037083/
+* https://tinderliker.com/
---
+++ robotinder.rb
@@ -0,0 +1,54 @@
+require "selenium-webdriver"
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "https://tinder.com"
+
+sleep 5
+
+# find the login with facebook button
+element = driver.find_element(:xpath, "//button[contains(.,'Login with Facebook')]")
+
+# push da button;opens a popup for fb login
+element.click
+
+# get all windows by ID
+all_window_handles = driver.window_handles()
+
+# save last window (popup) id
+popup_window = driver.window_handles.last
+
+# switch to popup window
+driver.switch_to.window(driver.window_handles.last)
+
+# fill the form
+driver.find_element(:id, 'email').send_keys# 'email'
+driver.find_element(:id, 'pass').send_keys# 'password'
+
+# find log In button
+#id="email"
+#name="email"
+element = driver.find_element(:name, "login")
+
+# You are not logged in to tinder
+element.click
+
+# switch to the first window
+driver.switch_to.window(driver.window_handles.last)
+
+sleep 3
+# next #1
+element = driver.find_element(:xpath, "//button[contains(.,'Next')]").click
+sleep 1
+element = driver.find_element(:xpath, "//button[contains(.,'Next')]").click
+sleep 1
+# great
+element = driver.find_element(:xpath, "//button[contains(.,'Great!')]").click
+sleep 1
+# ok got it
+element = driver.find_element(:xpath, "//button[contains(.,'Ok, got it!')]").click
+
+# initiate right swiping logic here
+
+puts driver.title
+
+#driver.quit
